cc111bd1566c9a21dbee0fa953152c279de93530
Carpmagik <daviswhill9@gmail.com>
2026-01-17 16:44:02 -0700

    lght

---
+++ ponder/mission.js
@@ -0,0 +1,32 @@
+
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+let border = document.getElementById('content');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+    let current = selectElem.value;
+    if (current == 'dark') {
+        // code for changes to colors and logo
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+        logo.setAttribute('src', 'images/byu-black.png');
+        border.style.border = '1px solid white';
+
+    } else if (current == 'light') {
+
+        // code for changes to colors and logo
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+        logo.setAttribute('src', 'images/byu-white.png');
+        border.style.border = '1px solid black';
+    }
+
+    else {
+        // default
+        document.body.style.backgroundColor = 'none';
+        border.style.border = '1px solid gray';
+    }
+}           
+                    
